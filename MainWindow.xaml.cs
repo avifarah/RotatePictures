5ffdd0f729e522674b6be32716408532fe9eac1a
Avi Farah <avi_farah@hotmail.com>
2017-08-23 22:57:04 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using RotetePictures.ViewModel;
+using System.Reflection;
+using System.Windows;
+
+namespace RotetePictures
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		public MainWindow()
+		{
+			log4net.Config.XmlConfigurator.Configure();
+
+			InitializeComponent();
+			DataContext = new MainWindowViewModel();
+
+			Log.Info(string.Empty);
+			Log.Info(string.Empty);
+		}
+	}
+}
